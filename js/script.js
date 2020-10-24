12ea1b47edbdf2f2d05fa573d19b7f57338e17df
Bridgetmprice <72258806+Bridgetmprice@users.noreply.github.com>
2020-10-24 13:23:19 +0200

    linters used
    fixed code as per errors in linting

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-console.log("Welcome to my portfolio site");
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Welcome to my portfolio site')
+/* eslint-disable */
+function menuToggle () {
+  /* eslint-enable */
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
